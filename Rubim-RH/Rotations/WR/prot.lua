3a94044a6ff3011d969db92fab238eee002c3a94
S0latium <steady@msn.com>
2018-07-25 18:49:17 -0400

    - Reworked Warrior defensives to now react based on incoming damage-per-second

--- Rubim-RH/Rotations/WR/prot.lua
+++ Rubim-RH/Rotations/WR/prot.lua
@@ -14,147 +14,177 @@ local Target = Unit.Target;
 local Spell = HL.Spell;
 local Item = HL.Item;
 
---TODO
---Berserker Rage: Implement cast while feared.
---Vengenace: Very Weak
---UnstoppableForce: Implement higher priority Thunderclap during Avatar
---Implement with new Rubim PvP logic
-
-local ProtSpells = RubimRH.Spell[73]
-
--- Items : Currently unused
-if not Item.Warrior then Item.Warrior = {}; end
-Item.Warrior.Protection = {};
-local I = Item.Warrior.Protection; -- Unused
-
-local T202PC, T204PC = HL.HasTier("T20"); -- Unused
-local T212PC, T214PC = HL.HasTier("T21"); -- Unused
+local ISpell = RubimRH.Spell[73]
 
 --- Preliminary APL based on WoWHead Rotation Priority for 8.0.1
 -- WoWHead Guide Referenced: http://www.wowhead.com/protection-warrior-rotation-guide
 local function APL()
-    -- Re-buff when Battle Shout is down
+    -- Battle Shout -> Re-buff when down
     -- TODO: Need to wait for GGLoader to include this texture
     -- if not Player:Buff(ProtSpells.BattleShout) and ProtSpells.BattleShout:IsReady() then return ProtSpells.BattleShout:Cast() end
 
     -- Player not in combat
     if not Player:AffectingCombat() then return 0, 462338 end
 
-    -- Update Surrounding Enemies
-    HL.GetEnemies("Melee")
-    HL.GetEnemies(8, true)
-    HL.GetEnemies(10, true)
-    HL.GetEnemies(12, true)
+    if Target:Exists()
+    and Player:CanAttack(Unit("target")) then
+        -- Update Surrounding Enemies
+        HL.GetEnemies("Melee")
+        HL.GetEnemies(8, true)
+        HL.GetEnemies(10, true)
+        HL.GetEnemies(12, true)
 
-    local IsTanking = Player:IsTankingAoE(8) or Player:IsTanking(Target) -- TODO: Implement logic for PvP scenarios : IsTanking returns false, yet Shield Block is still needed
-    local ThunderClapRadius = ProtSpells.CracklingThunder:IsAvailable() and 12 or 8
+        local IsTanking = Player:IsTankingAoE(8) or Player:IsTanking(Target) -- TODO: Implement logic for PvP scenarios : IsTanking returns false, yet Shield Block is still needed
+        local ThunderClapRadius = ISpell.CracklingThunder:IsAvailable() and 12 or 8
+        local IncomingDamage = select(1, RubimRH.getDMG("player"))
+        local NeedMinorHealing = ((IncomingDamage >= (Player:MaxHealth() * 0.05)) or Player:HealthPercentage() <= 50) and true or false -- Taking 5% max HP in DPS or <= 50% HP
+        local NeedBigHealing = ((IncomingDamage >= (Player:MaxHealth() * 0.1))) and true or false -- Taking 10% max HP in DPS
+        local PanicHeals = (Player:HealthPercentage() <= 30) and true or false
 
-    LeftCtrl = IsLeftControlKeyDown();
-    LeftShift = IsLeftShiftKeyDown();
+        local LeftCtrl = IsLeftControlKeyDown()
+        local LeftShift = IsLeftShiftKeyDown()
 
-    if LeftCtrl and LeftShift and ProtSpells.Shockwave:IsCastable(8) then
-        return ProtSpells.Shockwave:Cast()
-    end
+        -- Shovkwave -> Cast when left CTRL+Shift keys are pressed
+        if LeftCtrl and LeftShift and ISpell.Shockwave:IsCastable(8) then
+            return ISpell.Shockwave:Cast()
+        end
 
-    if ProtSpells.Pummel:IsReady("Melee")
-            and Target:IsInterruptible()
-            and Target:CastRemains() <= 0.5 then
-        return ProtSpells.Pummel:Cast()
-    end
+        -- Pummel -> 0.5 sec of cast has elapsed, or 1 second of channeling has elapsed
+        if ISpell.Pummel:IsReady("Melee")
+        and Target:IsInterruptible()
+        and (Target:CastRemains() <= 0.7
+            or (Target:IsChanneling()
+                and (Target:CastRemains() >= (Target:CastDuration() - 1)))) then
+            return ISpell.Pummel:Cast()
+        end
 
-    -- SHIELD BLOCK PRIMARY RAGE DUMP
-    if ProtSpells.ShieldBlock:IsCastable("Melee")
-            and Player:Rage() >= 30
-            and not Player:Buff(ProtSpells.ShieldBlockBuff)
-            and ((not ProtSpells.Bolster:IsAvailable()) or (ProtSpells.Bolster:IsAvailable() and not Player:Buff(ProtSpells.LastStand)))
-            and ProtSpells.ShieldBlock:ChargesFractional() >= 1
-            and IsTanking then -- TODO: See IsTanking note
-        return ProtSpells.ShieldBlock:Cast()
-    end
+        -- TODO: Berserker Rage: Implement cast while feared.
 
-    if ProtSpells.Avatar:IsCastable("Melee")
-            and Target:TimeToDie() >= 10
-            and Player:RageDeficit() >= 20 then
-        return ProtSpells.Avatar:Cast()
-    end
+        -- Shield Wall -> Panic Heal
+        if ISpell.ShieldWall:IsReady("Melee")
+        and PanicHeals
+        and (ISpell.Bolster:IsAvailable() and (not Player:Buff(ISpell.LastStand)))then
+            return ISpell.ShieldWall:Cast()
+        end
 
-    -- PvP Shield Bash
-    if ProtSpells.ShieldBash:IsCastable("Melee")
-            and Target:IsCasting() then
-        return ProtSpells.ShieldBash:Cast()
-    end
+        -- Last Stand -> Panic Heal
+        if ISpell.LastStand:IsReady("Melee")
+        and PanicHeals
+        and (not Player:Buff(ISpell.ShieldWall)) then
+            return ISpell.LastStand:Cast()
+        end
 
-    -- USE ON COOLDOWN WITH BOOMING VOICE
-    if ((ProtSpells.BoomingVoice:IsAvailable() and Player:Rage() <= 60) or Cache.EnemiesCount[ThunderClapRadius] >= 3)
-            and ProtSpells.DemoralizingShout:IsCastable("Melee") then
-        return ProtSpells.DemoralizingShout:Cast()
-    end
+        -- Shield Block -> Primary rage dump
+        if ISpell.ShieldBlock:IsReady("Melee")
+        and Player:Rage() >= 30
+        and not Player:Buff(ISpell.ShieldBlockBuff)
+        and ((not ISpell.Bolster:IsAvailable())
+            or (ISpell.Bolster:IsAvailable() and not Player:Buff(ISpell.LastStand)))
+        and ISpell.ShieldBlock:ChargesFractional() >= 1
+        and (NeedMinorHealing
+            or NeedBigHealing) then
+            return ISpell.ShieldBlock:Cast()
+        end
 
-    if ProtSpells.ShieldSlam:IsCastable("Melee")
-            and Player:RageDeficit() >= 15 then
-        return ProtSpells.ShieldSlam:Cast()
-    end
+        -- Avatar -> Cast when not in a group (solo conent), Target TTD >= 10, and we're at >= 20 rage deficit
+        if ISpell.Avatar:IsReady("Melee")
+                and ((Target:TimeToDie() >= 10) or (GetNumGroupMembers() == 0)) -- Use all the time in solo content
+                and Player:RageDeficit() >= 20 then
+            return ISpell.Avatar:Cast()
+        end
 
-    if ProtSpells.ThunderClap:IsCastable(ThunderClapRadius)
-            and Player:RageDeficit() >= 5 then
-        return ProtSpells.ThunderClap:Cast()
-    end
+        -- Shield Bash -> PvP usage
+        if ISpell.ShieldBash:IsReady("Melee")
+                and Target:IsCasting() then
+            return ISpell.ShieldBash:Cast()
+        end
 
-    -- Revenge Rage Dump
-    local RevengeDumpRage = ProtSpells.BoomingVoice:IsAvailable() and 60 or 80
-    if ProtSpells.Revenge:IsCastable("Melee")
-            and (((Player:Rage() >= RevengeDumpRage) or Player:Buff(ProtSpells.RevengeBuff))
-            or (ProtSpells.Revenge:IsCastable("Melee") and ProtSpells.Vengeance:IsAvailable() and Player:Buff(ProtSpells.VegeanceRV) and Player:Rage() >= 20)) then
-        return ProtSpells.Revenge:Cast()
-    end
+        -- Demoralizing Shout -> Use on CD with Boomking Shout
+        if ((ISpell.BoomingVoice:IsAvailable() and Player:Rage() <= 60)
+                or (Cache.EnemiesCount[ThunderClapRadius] >= 3)
+                or (GetNumGroupMembers() == 0))
+                and ISpell.DemoralizingShout:IsReady("Melee") then
+            return ISpell.DemoralizingShout:Cast()
+        end
 
-    if ProtSpells.ImpendingVictory:IsCastable("Melee")
-            and Player:HealthPercentage() <= 85 then
-        return ProtSpells.VictoryRush:Cast()
-    end
+        -- Impending Victory -> Cast when < 85% HP
+        if ISpell.ImpendingVictory:IsReady("Melee")
+                and Player:HealthPercentage() <= 85 then
+            return ISpell.VictoryRush:Cast()
+        end
 
-    if Player:Buff(ProtSpells.Victorious)
-            and ProtSpells.VictoryRush:IsCastable("Melee")
-            and Player:HealthPercentage() <= 85 then
-        return ProtSpells.VictoryRush:Cast()
-    end
+        -- Victory Rush -> Cast when < 85% HP
+        if Player:Buff(ISpell.Victorious)
+                and ISpell.VictoryRush:IsReady("Melee")
+                and Player:HealthPercentage() <= 85 then
+            return ISpell.VictoryRush:Cast()
+        end
 
-    if ProtSpells.Ravager:IsCastable("Melee")
-            and Cache.EnemiesCount[8] >= 3 then
-        return ProtSpells.Ravager:Cast()
-    end
+        -- Shield Slam
+        if ISpell.ShieldSlam:IsReady("Melee")
+                and Player:RageDeficit() >= 15 then
+            return ISpell.ShieldSlam:Cast()
+        end
 
-    if ProtSpells.ShieldBash:IsCastable("Melee") then
-        return ProtSpells.ShieldBash:Cast()
-    end
+        -- ThunderClap
+        if ISpell.ThunderClap:IsReady(ThunderClapRadius)
+                and Player:RageDeficit() >= 5 then
+            return ISpell.ThunderClap:Cast()
+        end
 
-    if Player:Buff(ProtSpells.VegeanceIP)
-            and Player:Rage() >= ((40 / 3) * 2)
-            and not Player:Buff(ProtSpells.IgnorePain) then
-        return ProtSpells.IgnorePain:Cast()
-    end
+        -- Revenge Rage Dump
+        local RevengeDumpRage = ISpell.BoomingVoice:IsAvailable() and 60 or 80
+        if ISpell.Revenge:IsReady("Melee")
+                and (((Player:Rage() >= RevengeDumpRage)
+                    or Player:Buff(ISpell.RevengeBuff))
+                    or (ISpell.Revenge:IsReady("Melee") and Player:Buff(ISpell.VegeanceRV) and Player:Rage() >= 20)) then
+            return ISpell.Revenge:Cast()
+        end
 
-    if ProtSpells.Revenge:IsCastable("Melee")
-            and ProtSpells.ShieldBlock:ChargesFractional() < 0.6
-            and Player:Rage() >= 30 then
-        return ProtSpells.Revenge:Cast()
-    end
+        -- Ravager -> AoE scenarios
+        if ISpell.Ravager:IsReady("Melee")
+                and Cache.EnemiesCount[8] >= 3 then
+            return ISpell.Ravager:Cast()
+        end
 
-    if Player:Buff(ProtSpells.Victorious)
-            and Player:BuffRemains(ProtSpells.Victorious) <= 2
-            and ProtSpells.VictoryRush:IsCastable("Melee") then
-        return ProtSpells.VictoryRush:Cast()
-    end
+        -- Shield Bash -> Target not casting / lower priority
+        if ISpell.ShieldBash:IsReady("Melee") then
+            return ISpell.ShieldBash:Cast()
+        end
 
-    if ProtSpells.IgnorePain:IsCastable("Melee")
-            and Player:Rage() >= 40
-            and not Player:Buff(ProtSpells.IgnorePain)
-            and IsTanking then -- TODO: See IsTanking note
-        return ProtSpells.IgnorePain:Cast()
-    end
+        -- Ignore Pain -> Vengeance Ignore Pain
+        if ISpell.IgnorePain:IsReady("Melee")
+                and Player:Buff(ISpell.VegeanceIP)
+                and Player:Rage() >= ((40 / 3) * 2)
+                and not Player:Buff(ISpell.IgnorePain) then
+            return ISpell.IgnorePain:Cast()
+        end
 
-    if ProtSpells.Devastate:IsCastable("Melee") then
-        return ProtSpells.Devastate:Cast()
+        -- Revenge -> Rage dump
+        if ISpell.Revenge:IsReady("Melee")
+                and ISpell.ShieldBlock:ChargesFractional() < 0.6
+                and Player:Rage() >= 30 then
+            return ISpell.Revenge:Cast()
+        end
+
+        -- Victory Rush -> Buff about to expire
+        if Player:Buff(ISpell.Victorious)
+                and Player:BuffRemains(ISpell.Victorious) <= 2
+                and ISpell.VictoryRush:IsReady("Melee") then
+            return ISpell.VictoryRush:Cast()
+        end
+
+        -- Ignore Pain -> Only cast in place of Devastate
+        if ISpell.IgnorePain:IsReady("Melee")
+                and Player:Rage() >= 40
+                and not Player:Buff(ISpell.IgnorePain)
+                and (NeedMinorHealing or NeedBigHealing) then -- TODO: See IsTanking note
+            return ISpell.IgnorePain:Cast()
+        end
+
+        if ISpell.Devastate:IsCastable("Melee") then
+            return ISpell.Devastate:Cast()
+        end
     end
 
     return 0, 975743
