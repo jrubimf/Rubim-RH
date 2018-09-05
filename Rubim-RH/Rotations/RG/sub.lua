64e84d6858462d04d6914a4fb1ba7903944379ec
Rubim <jrubimf@gmail.com>
2018-09-05 00:03:12 -0300

    Sub fix

--- Rubim-RH/Rotations/RG/sub.lua
+++ Rubim-RH/Rotations/RG/sub.lua
@@ -198,7 +198,7 @@ local function Finish ()
             return S.Nightblade:Cast()
         end
         -- actions.finish+=/nightblade,cycle_targets=1,if=spell_targets.shuriken_storm>=2&(spell_targets.shuriken_storm<=5|talent.secret_technique.enabled)&!buff.shadow_dance.up&target.time_to_die>=(5+(2*combo_points))&refreshable
-        if S.Nightblade:IsReady() and (Cache.EnemiesCount[10] >= 2 and (S.SecretTechnique:IsAvailable() or S.NightsVengeancePower:AzeriteEnabled() or Cache.EnemiesCount[10] <= 5) and not Player:BuffP(S.ShadowDanceBuff) and Target:TimeToDie() >= (5 + (2 * Player:ComboPoints())) and Target:DebuffRefreshableC(S.NightbladeDebuff)) then
+        if S.Nightblade:IsReady() and (Cache.EnemiesCount[10] >= 2 and (S.SecretTechnique:IsAvailable() or S.NightsVengeancePower:AzeriteEnabled() or Cache.EnemiesCount[10] <= 5) and not Player:BuffP(S.ShadowDanceBuff) and Target:TimeToDie() >= (5 + (2 * Player:ComboPoints())) and Target:DebuffRefreshableC(S.Nightblade)) then
             return S.Nightblade:Cast()
         end
 
