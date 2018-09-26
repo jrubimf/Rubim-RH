ee43d724e2d9033ed078b62dc2c25cb73aa918b3
Rubim <jrubimf@gmail.com>
2018-09-26 15:35:41 -0300

    Opener disable test

--- Rubim-RH/Rotations/PL/ret.lua
+++ Rubim-RH/Rotations/PL/ret.lua
@@ -351,9 +351,9 @@ local function APL()
         end
     end
 
-    if HL.CombatTime() < 2 and opener ~= nil and RubimRH.CDsON() and Target:IsInRange("Melee") then
-        return Opener()
-    end
+    --if HL.CombatTime() < 2 and opener ~= nil and RubimRH.CDsON() and Target:IsInRange("Melee") then
+        --return Opener()
+    --end
 
     if S.FlashOfLight:IsReady() and Player:BuffStack(S.SelfLessHealerBuff) == 4 and Player:HealthPercentage() <= RubimRH.db.profile[70].sk2 and Player:StoppedFor() >= 0.5 then
         return S.FlashOfLight:Cast()
