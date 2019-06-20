4e6714f46380070f4208d6fa6bdcad6b6d7788d0
rollfacedk <rollfacedk@gmail.com>
2019-06-20 12:45:40 -0400

    Enslaughter Update
    simc update and Avenger's Shield interrupt only (optional). It's good for solo interrupt 2nd boss in shrine of the storm.

--- Rubim-RH/Rotations/PL/prot.lua
+++ Rubim-RH/Rotations/PL/prot.lua
@@ -18,13 +18,13 @@ local Item = HL.Item
 
 -- Spells
 RubimRH.Spell[66] = {
+    BastionOfLight = Spell(204035),
     Seraphim = Spell(152262),
     ShieldoftheRighteous = Spell(53600),
     AvengingWrath = Spell(31884),
     SeraphimBuff = Spell(152262),
     AvengingWrathBuff = Spell(31884),
     AvengersValorBuff = Spell(197561),
-    AvengerShield = Spell(31935),
     LightsJudgment = Spell(255647),
     AvengersShield = Spell(31935),
     Judgment = Spell(275779),
@@ -137,12 +137,19 @@ local function APL()
         return 0, 462338
     end
 
-    if S.Rebuke:IsReady(30) and RubimRH.db.profile.mainOption.useInterrupts and Target:IsInterruptible() then
+    if S.AvengersShield:IsReady() and Target:IsInterruptible() and RubimRH.ASInterruptON() then
+        return S.AvengersShield:Cast()
+    end
+    if S.Rebuke:IsReady() and RubimRH.db.profile.mainOption.useInterrupts and Target:IsInterruptible() then
         return S.Rebuke:Cast()
     end
 
-    if (S.LightofTheProtector:IsReady() or S.HandOfTheProtector:IsReady()) and Player:HealthPercentage() <= RubimRH.db.profile[66].sk1 then
+    if (S.LightofTheProtector:IsReady() or S.HandOfTheProtector:IsReady()) and Player:HealthPercentage() <= RubimRH.db.profile[66].sk1 and not Player:HealingAbsorbed() then
         return S.LightofTheProtector:Cast()
+    end
+
+    if S.ShieldoftheRighteous:IsReady() and not Player:BuffP(S.ShieldoftheRighteousBuff) and not Player:BuffP(S.AvengingWrathBuff) and Player:HealthPercentage() < 75 and RubimRH.ASInterruptON() then
+        return S.ShieldoftheRighteous:Cast()
     end
 
     -- auto_attack
@@ -154,7 +161,10 @@ local function APL()
     if S.AvengingWrath:IsReady() and (Player:Buff(S.SeraphimBuff) or S.Seraphim:CooldownRemains() < 2 or not S.Seraphim:IsAvailable()) then
         return S.AvengingWrath:Cast()
     end
-
+    -- bastion_of_light,if=cooldown.shield_of_the_righteous.charges_fractional<=0.5
+    if S.BastionOfLight:IsReady() and S.ShieldoftheRighteous:ChargesFractional() <= 0.5 then
+        return S.BastionOfLight:Cast()
+    end
     -- Mouseover Functionality
     local MouseoverUnit = (UnitExists("mouseover") and UnitIsFriend("player", "mouseover") and (UnitGUID("mouseover") ~= UnitGUID("player"))) and Unit("mouseover") or nil
     if MouseoverUnit then
@@ -203,10 +213,7 @@ local function APL()
     if S.ShieldoftheRighteous:IsReady() and ((Player:Buff(S.AvengersValorBuff) and S.ShieldoftheRighteous:ChargesFractional() >= 2.5) and (S.Seraphim:CooldownRemains() > Player:GCD() or not S.Seraphim:IsAvailable())) then
         return S.ShieldoftheRighteous:Cast()
     end
-    -- shield_of_the_righteous,if=(cooldown.shield_of_the_righteous.charges_fractional=3&cooldown.avenger_shield.remains>(2*gcd))
-    if S.ShieldoftheRighteous:IsReady() and ((S.ShieldoftheRighteous:ChargesFractional() == 3 and S.AvengerShield:CooldownRemains() > (2 * Player:GCD()))) then
-        return S.ShieldoftheRighteous:Cast()
-    end
+    
     -- shield_of_the_righteous,if=(buff.avenging_wrath.up&!talent.seraphim.enabled)|buff.seraphim.up&buff.avengers_valor.up
     if S.ShieldoftheRighteous:IsReady() and ((Player:Buff(S.AvengingWrathBuff) and not S.Seraphim:IsAvailable()) or Player:Buff(S.SeraphimBuff) and Player:Buff(S.AvengersValorBuff)) then
         return S.ShieldoftheRighteous:Cast()
@@ -215,43 +222,41 @@ local function APL()
     if S.ShieldoftheRighteous:IsReady() and ((Player:Buff(S.AvengingWrathBuff) and Player:BuffRemains(S.AvengingWrathBuff) < 4 and not S.Seraphim:IsAvailable()) or (Player:BuffRemains(S.SeraphimBuff) < 4 and Player:Buff(S.SeraphimBuff))) then
         return S.ShieldoftheRighteous:Cast()
     end
-    -- use_items,if=buff.seraphim.up|!talent.seraphim.enabled
-
-    -- avengers_shield,if=(cooldown.shield_of_the_righteous.charges_fractional>2.5&!buff.avengers_valor.up)|active_enemies>=2
-    if S.AvengersShield:IsReady() and ((S.ShieldoftheRighteous:ChargesFractional() > 2.5 and not Player:Buff(S.AvengersValorBuff)) or Cache.EnemiesCount[8] >= 2) then
-        return S.AvengersShield:Cast()
-    end
-    -- judgment,if=(cooldown.judgment.remains<gcd&cooldown.judgment.charges_fractional>1)|!talent.crusaders_judgment.enabled
-    if S.Judgment:IsReady() and ((S.Judgment:CooldownRemains() < Player:GCD() and S.Judgment:ChargesFractional() > 1) or not S.CrusadersJudgment:IsAvailable()) then
-        return S.Judgment:Cast()
-    end
-
-    if S.AvengersShield:IsReady() then
-        return S.AvengersShield:Cast()
-    end
-
-    -- consecration,if=(cooldown.judgment.remains<=gcd&!talent.crusaders_judgment.enabled)|cooldown.avenger_shield.remains<=gcd&consecration.remains<gcd
-    if S.Consecration:IsReady() and ((S.Judgment:CooldownRemains() <= Player:GCD() and not S.CrusadersJudgment:IsAvailable()) or S.AvengerShield:CooldownRemains() <= Player:GCD() and ConcerationTime() < Player:GCD()) then
-        return S.Consecration:Cast()
-    end
-    -- consecration,if=!talent.crusaders_judgment.enabled&consecration.remains<(cooldown.judgment.remains+cooldown.avengers_shield.remains)&consecration.remains<3*gcd
-    if S.Consecration:IsReady() and (not S.CrusadersJudgment:IsAvailable() and ConcerationTime() < (S.Judgment:CooldownRemains() + S.AvengersShield:CooldownRemains()) and ConcerationTime() < 3 * Player:GCD()) then
-        return S.Consecration:Cast()
-    end
-
-    -- judgment
-    if S.Judgment:IsReady() then
-        return S.Judgment:Cast()
-    end
-
-
-    -- lights_judgment,if=!talent.seraphim.enabled|buff.seraphim.up
-    if S.LightsJudgment:IsReady() and RubimRH.CDsON() and (not S.Seraphim:IsAvailable() or Player:Buff(S.SeraphimBuff)) then
+    -- lights_judgment,if=buff.seraphim.up&buff.seraphim.remains<3
+    if S.LightsJudgment:IsReady() and RubimRH.CDsON() and (Player:Buff(S.SeraphimBuff) and Player:BuffPRemains(S.SeraphimBuff) < 3) then
         return S.LightsJudgment:Cast()
     end
 
+    -- consecration,if=!consecration.up
+    if S.Consecration:IsReady() and not Player:BuffP(S.ConsecrationBuff)   then
+        return S.Consecration:Cast()
+    end
 
-    -- blessed_hammer
+    -- avengers_shield,if=(cooldown.shield_of_the_righteous.charges_fractional>2.5&!buff.avengers_valor.up)|active_enemies>=2
+    if S.AvengersShield:IsReady() and ((S.ShieldoftheRighteous:ChargesFractional() > 2.5 and not Player:Buff(S.AvengersValorBuff)) or Cache.EnemiesCount[8] >= 2) and not RubimRH.ASInterruptON() then
+        return S.AvengersShield:Cast()
+    end
+    -- judgment,if=(cooldown.judgment.remains<gcd&cooldown.judgment.charges_fractional>1&cooldown_react)|!talent.crusaders_judgment.enabled
+    if S.Judgment:IsReady() and ((S.Judgment:CooldownRemains() < Player:GCD() and S.Judgment:ChargesFractional() > 1 and S.Judgment:CooldownUpP()) or not S.CrusadersJudgment:IsAvailable()) then
+        return S.Judgment:Cast()
+    end
+
+    -- avengers_shield,if=cooldown_react
+    if S.AvengersShield:IsReady() and S.AvengersShield:CooldownUpP() and not RubimRH.ASInterruptON() then
+        return S.AvengersShield:Cast()
+    end
+
+    -- judgment,if=cooldown_react|!talent.crusaders_judgment.enabled
+    if S.Judgment:IsReady() and (S.Judgment:CooldownUpP() or not S.CrusadersJudgment:IsAvailable()) then
+        return S.Judgment:Cast()
+    end
+
+    -- lights_judgment,if=!talent.seraphim.enabled|buff.seraphim.up
+    if S.LightsJudgment:IsReady() and RubimRH.CDsON() and (S.Seraphim:IsAvailable() or Player:Buff(S.SeraphimBuff)) then
+        return S.LightsJudgment:Cast()
+    end
+    
+    -- actions+=/blessed_hammer,strikes=3
     if S.BlessedHammer:IsReady() then
         return S.BlessedHammer:Cast()
     end
@@ -259,10 +264,12 @@ local function APL()
     if S.HammeroftheRighteous:IsReady() then
         return S.HammeroftheRighteous:Cast()
     end
+
     -- consecration
-    if S.Consecration:IsReady() and not Player:Buff(S.ConsecrationBuff) then
+    if S.Consecration:IsReady()  then
         return S.Consecration:Cast()
     end
+
     return 0, 135328
 end
 
