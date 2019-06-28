08e788f008d54e8538b0e269639de0ef601d2b7c
rollfacedk <rollfacedk@gmail.com>
2019-06-27 20:15:23 -0400

    [Vengeance] Rotation Improvement.

--- Rubim-RH/Rotations/DH/veng.lua
+++ Rubim-RH/Rotations/DH/veng.lua
@@ -23,6 +23,7 @@ RubimRH.Spell[581] = {
     Sever = Spell(235964),
     Shear = Spell(203782),
     SigilofFlame = Spell(204596),
+    SigilofFlame2 = Spell(204513),
     SpiritBomb = Spell(247454),
     SoulCleave = Spell(228477),
     SoulFragments = Spell(203981),
@@ -36,7 +37,7 @@ RubimRH.Spell[581] = {
     Metamorphosis = Spell(187827),
     SoulBarrier = Spell(263648),
     -- Utility
-    ConsumeMagic = Spell(183752),
+    ConsumeMagic = Spell(278326),
     InfernalStrike = Spell(189110),
     Disrupt = Spell(183752),
     CharredFlesh = Spell(264002),
@@ -44,36 +45,38 @@ RubimRH.Spell[581] = {
     SigilofMisery = Spell(207684),
     SigilofChains = Spell(202138),
     ArcaneTorrent = Spell(202719),
-	
-  --8.2 Essences
-  UnleashHeartOfAzeroth = Spell(280431),
-  BloodOfTheEnemy       = Spell(297108),
-  BloodOfTheEnemy2      = Spell(298273),
-  BloodOfTheEnemy3      = Spell(298277),
-  ConcentratedFlame     = Spell(295373),
-  ConcentratedFlame2    = Spell(299349),
-  ConcentratedFlame3    = Spell(299353),
-  GuardianOfAzeroth     = Spell(295840),
-  GuardianOfAzeroth2    = Spell(299355),
-  GuardianOfAzeroth3    = Spell(299358),
-  FocusedAzeriteBeam    = Spell(295258),
-  FocusedAzeriteBeam2   = Spell(299336),
-  FocusedAzeriteBeam3   = Spell(299338),
-  PurifyingBlast        = Spell(295337),
-  PurifyingBlast2       = Spell(299345),
-  PurifyingBlast3       = Spell(299347),
-  TheUnboundForce       = Spell(298452),
-  TheUnboundForce2      = Spell(299376),
-  TheUnboundForce3      = Spell(299378),
-  RippleInSpace         = Spell(302731),
-  RippleInSpace2        = Spell(302982),
-  RippleInSpace3        = Spell(302983),
-  WorldveinResonance    = Spell(295186),
-  WorldveinResonance2   = Spell(298628),
-  WorldveinResonance3   = Spell(299334),
-  MemoryOfLucidDreams   = Spell(298357),
-  MemoryOfLucidDreams2  = Spell(299372),
-  MemoryOfLucidDreams3  = Spell(299374),
+    ConcentratedSigils = Spell(207666),
+    FallOut = Spell(22766),
+
+    --8.2 Essences
+    HoA                   = Spell(280431),
+    BloodOfTheEnemy       = Spell(297108),
+    BloodOfTheEnemy2      = Spell(298273),
+    BloodOfTheEnemy3      = Spell(298277),
+    ConcentratedFlame     = Spell(295373),
+    ConcentratedFlame2    = Spell(299349),
+    ConcentratedFlame3    = Spell(299353),
+    GuardianOfAzeroth     = Spell(295840),
+    GuardianOfAzeroth2    = Spell(299355),
+    GuardianOfAzeroth3    = Spell(299358),
+    FocusedAzeriteBeam    = Spell(295258),
+    FocusedAzeriteBeam2   = Spell(299336),
+    FocusedAzeriteBeam3   = Spell(299338),
+    PurifyingBlast        = Spell(295337),
+    PurifyingBlast2       = Spell(299345),
+    PurifyingBlast3       = Spell(299347),
+    TheUnboundForce       = Spell(298452),
+    TheUnboundForce2      = Spell(299376),
+    TheUnboundForce3      = Spell(299378),
+    RippleInSpace         = Spell(302731),
+    RippleInSpace2        = Spell(302982),
+    RippleInSpace3        = Spell(302983),
+    WorldveinResonance    = Spell(295186),
+    WorldveinResonance2   = Spell(298628),
+    WorldveinResonance3   = Spell(299334),
+    MemoryOfLucidDreams   = Spell(298357),
+    MemoryOfLucidDreams2  = Spell(299372),
+    MemoryOfLucidDreams3  = Spell(299374),
 }
 
 local S = RubimRH.Spell[581]
@@ -81,11 +84,31 @@ local S = RubimRH.Spell[581]
 S.Fracture.TextureSpellID = { 279450 }
 S.Sever.TextureSpellID = { 279450 }
 --S.ImmolationAura.TextureSpellID = { 202137 }
--- Rotation Var
-local ShouldReturn; -- Used to get the return string
+
 local T202PC, T204PC = HL.HasTier("T20");
 local T212PC, T214PC = HL.HasTier("T21");
 -- APL Main
+
+local function DetermineEssenceRanks()
+    S.BloodOfTheEnemy = S.BloodOfTheEnemy2:IsAvailable() and S.BloodOfTheEnemy2 or S.BloodOfTheEnemy;
+    S.BloodOfTheEnemy = S.BloodOfTheEnemy3:IsAvailable() and S.BloodOfTheEnemy3 or S.BloodOfTheEnemy;
+    S.MemoryOfLucidDreams = S.MemoryOfLucidDreams2:IsAvailable() and S.MemoryOfLucidDreams2 or S.MemoryOfLucidDreams;
+    S.MemoryOfLucidDreams = S.MemoryOfLucidDreams3:IsAvailable() and S.MemoryOfLucidDreams3 or S.MemoryOfLucidDreams;
+    S.PurifyingBlast = S.PurifyingBlast2:IsAvailable() and S.PurifyingBlast2 or S.PurifyingBlast;
+    S.PurifyingBlast = S.PurifyingBlast3:IsAvailable() and S.PurifyingBlast3 or S.PurifyingBlast;
+    S.RippleInSpace = S.RippleInSpace2:IsAvailable() and S.RippleInSpace2 or S.RippleInSpace;
+    S.RippleInSpace = S.RippleInSpace3:IsAvailable() and S.RippleInSpace3 or S.RippleInSpace;
+    S.ConcentratedFlame = S.ConcentratedFlame2:IsAvailable() and S.ConcentratedFlame2 or S.ConcentratedFlame;
+    S.ConcentratedFlame = S.ConcentratedFlame3:IsAvailable() and S.ConcentratedFlame3 or S.ConcentratedFlame;
+    S.TheUnboundForce = S.TheUnboundForce2:IsAvailable() and S.TheUnboundForce2 or S.TheUnboundForce;
+    S.TheUnboundForce = S.TheUnboundForce3:IsAvailable() and S.TheUnboundForce3 or S.TheUnboundForce;
+    S.WorldveinResonance = S.WorldveinResonance2:IsAvailable() and S.WorldveinResonance2 or S.WorldveinResonance;
+    S.WorldveinResonance = S.WorldveinResonance3:IsAvailable() and S.WorldveinResonance3 or S.WorldveinResonance;
+    S.FocusedAzeriteBeam = S.FocusedAzeriteBeam2:IsAvailable() and S.FocusedAzeriteBeam2 or S.FocusedAzeriteBeam;
+    S.FocusedAzeriteBeam = S.FocusedAzeriteBeam3:IsAvailable() and S.FocusedAzeriteBeam3 or S.FocusedAzeriteBeam;
+    S.GuardianOfAzeroth = S.GuardianOfAzeroth2:IsAvailable() and S.GuardianOfAzeroth2 or S.GuardianOfAzeroth;
+    S.GuardianOfAzeroth = S.GuardianOfAzeroth3:IsAvailable() and S.GuardianOfAzeroth3 or S.GuardianOfAzeroth;
+end
 
 local EnemyRanges = { "Melee", 8, 10, 20, 30, 40 }
 local function UpdateRanges()
@@ -94,92 +117,55 @@ local function UpdateRanges()
     end
 end
 
+local OffensiveCDs = {
+    S.ConcentratedFlame,
+    S.MemoryOfLucidDreams,
+    S.RippleInSpace,
+    S.WorldveinResonance,
 
-RubimRH.Listener:Add('Vengeance', 'UNIT_SPELLCAST_SUCCEEDED', function(...)
-    local unit, castGUID, spellID = ...
+}
 
-    if unit == "player" then
-        if spellID == 189110 then
-            S.InfernalStrike.LastCastTime = GetTime()
+local function UpdateCDs()
+    RubimRH.db.profile.mainOption.disabledSpellsCD = {}
+    if RubimRH.CDsON() then
+        for i, spell in pairs(OffensiveCDs) do
+            if not spell:IsEnabledCD() then
+                RubimRH.delSpellDisabledCD(spell:ID())
+            end
+        end
+
+    end
+    if not RubimRH.CDsON() then
+        for i, spell in pairs(OffensiveCDs) do
+            if spell:IsEnabledCD() then
+                RubimRH.addSpellDisabledCD(spell:ID())
+            end
         end
     end
-
-    if unit == "player" then
-        if spellID == 228477 then
-            S.SoulCleave.LastCastTime = GetTime()
-        end
-    end
-    
-
-end)
-
-local function DetermineEssenceRanks()
-  S.BloodOfTheEnemy = S.BloodOfTheEnemy2:IsAvailable() and S.BloodOfTheEnemy2 or S.BloodOfTheEnemy
-  S.BloodOfTheEnemy = S.BloodOfTheEnemy3:IsAvailable() and S.BloodOfTheEnemy3 or S.BloodOfTheEnemy
-  S.MemoryOfLucidDreams = S.MemoryOfLucidDreams2:IsAvailable() and S.MemoryOfLucidDreams2 or S.MemoryOfLucidDreams
-  S.MemoryOfLucidDreams = S.MemoryOfLucidDreams3:IsAvailable() and S.MemoryOfLucidDreams3 or S.MemoryOfLucidDreams
-  S.PurifyingBlast = S.PurifyingBlast2:IsAvailable() and S.PurifyingBlast2 or S.PurifyingBlast
-  S.PurifyingBlast = S.PurifyingBlast3:IsAvailable() and S.PurifyingBlast3 or S.PurifyingBlast
-  S.RippleInSpace = S.RippleInSpace2:IsAvailable() and S.RippleInSpace2 or S.RippleInSpace
-  S.RippleInSpace = S.RippleInSpace3:IsAvailable() and S.RippleInSpace3 or S.RippleInSpace
-  S.ConcentratedFlame = S.ConcentratedFlame2:IsAvailable() and S.ConcentratedFlame2 or S.ConcentratedFlame
-  S.ConcentratedFlame = S.ConcentratedFlame3:IsAvailable() and S.ConcentratedFlame3 or S.ConcentratedFlame
-  S.TheUnboundForce = S.TheUnboundForce2:IsAvailable() and S.TheUnboundForce2 or S.TheUnboundForce
-  S.TheUnboundForce = S.TheUnboundForce3:IsAvailable() and S.TheUnboundForce3 or S.TheUnboundForce
-  S.WorldveinResonance = S.WorldveinResonance2:IsAvailable() and S.WorldveinResonance2 or S.WorldveinResonance
-  S.WorldveinResonance = S.WorldveinResonance3:IsAvailable() and S.WorldveinResonance3 or S.WorldveinResonance
-  S.FocusedAzeriteBeam = S.FocusedAzeriteBeam2:IsAvailable() and S.FocusedAzeriteBeam2 or S.FocusedAzeriteBeam
-  S.FocusedAzeriteBeam = S.FocusedAzeriteBeam3:IsAvailable() and S.FocusedAzeriteBeam3 or S.FocusedAzeriteBeam
 end
 
--- # Essences
-local function Essences()
-  -- blood_of_the_enemy
-  if S.BloodOfTheEnemy:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- concentrated_flame
-  if S.ConcentratedFlame:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- guardian_of_azeroth
-  if S.GuardianOfAzeroth:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- focused_azerite_beam
-  if S.FocusedAzeriteBeam:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- purifying_blast
-  if S.PurifyingBlast:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- the_unbound_force
-  if S.TheUnboundForce:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- ripple_in_space
-  if S.RippleInSpace:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- worldvein_resonance
-  if S.WorldveinResonance:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  -- memory_of_lucid_dreams,if=fury<40&buff.metamorphosis.up
-  if S.MemoryOfLucidDreams:IsCastableP() then
-    return S.UnleashHeartOfAzeroth:Cast()
-  end
-  return false
-end
 
 local function APL()
     local Precombat, Brand, Defensives, Normal
     UpdateRanges()
+    UpdateCDs()
+    DetermineEssenceRanks()
+    -- It helps not to waste SoulFragment
+    local DelayAction =
+        S.Fracture:TimeSinceLastCast() > Player:GCD()
+    and S.Shear:TimeSinceLastCast() > Player:GCD() 
+    and S.InfernalStrike:TimeSinceLastCast() > Player:GCD()
+    and S.FelDevastation:TimeSinceLastCast() > Player:GCD()
+    and S.Felblade:TimeSinceLastCast() > Player:GCD()
+    and S.ImmolationAura:TimeSinceLastCast() > Player:GCD()
+    and S.FieryBrand:TimeSinceLastCast() > Player:GCD()
+    and S.ThrowGlaive:TimeSinceLastCast() > Player:GCD()
+    and S.SoulBarrier:TimeSinceLastCast() > Player:GCD()
+    and S.SigilofFlame:TimeSinceLastCast() > Player:GCD()
+    and S.SigilofFlame2:TimeSinceLastCast() > Player:GCD();
 
     local SoulFragments = Player:BuffStack(S.SoulFragments);
     local IsTanking = Player:IsTankingAoE(8) or Player:IsTanking(Target);
-
     Precombat = function()
         -- flask
         -- augmentation
@@ -203,12 +189,12 @@ local function APL()
         end
 
         --actions.brand+=/infernal_strike,if=cooldown.fiery_brand.remains=0
-        if S.InfernalStrike:TimeSinceLastCast() > 1 and S.InfernalStrike:IsReady() and S.FieryBrand:CooldownRemains() == 0 and S.InfernalStrike:TimeSinceLastCast() > 2 and S.InfernalStrike:IsReady("Melee") and S.InfernalStrike:ChargesFractional() >= 2.0 - Player:GCD()/10 then
+        if S.InfernalStrike:TimeSinceLastCast() > 1 and Cache.EnemiesCount[8] >= 1 and S.InfernalStrike:IsReady() and S.FieryBrand:CooldownRemains() == 0 and S.InfernalStrike:TimeSinceLastCast() > 2 and S.InfernalStrike:IsReady("Melee") and S.InfernalStrike:ChargesFractional() >= 2.0 - Player:GCD()/10 then
             return S.InfernalStrike:Cast()
         end
 
         --actions.brand+=/fiery_brandF
-        if S.FieryBrand:IsReady("Melee") then
+        if S.FieryBrand:IsReady('Melee') then
             return S.FieryBrand:Cast()
         end
 
@@ -228,7 +214,7 @@ local function APL()
         end
 
         --actions.brand+=/sigil_of_flame,if=dot.fiery_brand.ticking
-        if S.SigilofFlame:IsReady() and Target:Debuff(S.FieryBrand) then
+        if S.SigilofFlame:IsReadyMorph() and Target:Debuff(S.FieryBrand) then
             return S.SigilofFlame:Cast()
         end
     end
@@ -236,13 +222,13 @@ local function APL()
     Defensives = function()
         --# Defensives
         --actions.defensives=demon_spikes
-        if S.DemonSpikes:IsReady("Melee") and Player:Pain() >= 20 and not Player:Buff(S.DemonSpikesBuff) and IsTanking and not Player:HealingAbsorbed() and S.DemonSpikes:ChargesFractional() >= 1.8 then
+        if S.DemonSpikes:IsReady() and not Player:Buff(S.DemonSpikesBuff) and IsTanking and Player:HealthPercentage() < 70 then
             return S.DemonSpikes:Cast()
         end
 
         --actions.defensives+=/metamorphosis
         --actions.defensives+=/fiery_brand
-        if S.FieryBrand:IsReady("Melee") then
+        if S.FieryBrand:IsReady('Melee') then
             return S.FieryBrand:Cast()
         end
     end
@@ -250,12 +236,12 @@ local function APL()
     Normal = function()
         --# Normal Rotation
         --actions.normal=infernal_strike
-        if S.InfernalStrike:TimeSinceLastCast() > 1 and S.InfernalStrike:IsReady("Melee") and S.InfernalStrike:ChargesFractional() >= 2.0 - Player:GCD()/10 then
+        if Cache.EnemiesCount[8] > 0 and S.InfernalStrike:TimeSinceLastCast() > 1 and S.InfernalStrike:IsReady() and S.InfernalStrike:ChargesFractional() >= 2.0 - Player:GCD()/10 then
             return S.InfernalStrike:Cast()
         end
 
         --actions.normal+=/spirit_bomb,if=soul_fragments>=4
-        if S.SpiritBomb:IsReady() and SoulFragments >= 4 then
+        if Cache.EnemiesCount[8] > 0 and S.SpiritBomb:IsReady() and SoulFragments >= 4 then
             return S.SpiritBomb:Cast()
         end
 
@@ -265,42 +251,59 @@ local function APL()
         end
 
         --actions.normal+=/soul_cleave,if=talent.spirit_bomb.enabled&soul_fragments=0
-        if S.SoulCleave:IsReady("Melee") and S.SpiritBomb:IsAvailable() and SoulFragments == 0 and S.SoulCleave:TimeSinceLastCast() > Player:GCD() * 0.8  then
+        if S.SoulCleave:IsReady("Melee") and S.SpiritBomb:IsAvailable() and SoulFragments == 0 
+            and DelayAction then
             return S.SoulCleave:Cast()
         end
-
         --actions.normal+=/immolation_aura,if=pain<=90
-        if S.ImmolationAura:IsReady() and Cache.EnemiesCount[8] >= 1 and Player:Pain() <= 90 then
+        if Cache.EnemiesCount[8] > 0 and S.ImmolationAura:IsReady() and Player:Pain() <= 90 then
             return S.ImmolationAura:Cast()
         end
 
+        -- concentrated_flame
+        if S.ConcentratedFlame:IsReady() then
+            return S.HoA:Cast()
+        end
+        -- ripple_in_space
+        if S.RippleInSpace:IsReady() then
+            return S.HoA:Cast()
+        end
+        -- worldvein_resonance
+        if S.WorldveinResonance:IsReady() then
+            return S.HoA:Cast()
+        end
+        -- memory_of_lucid_dreams
+        if S.MemoryOfLucidDreams:IsReady() then
+            return S.HoA:Cast()
+        end
+
         --actions.normal+=/felblade,if=pain<=70
-        if S.Felblade:IsReady() and Player:Pain() <= 70 then
+        if S.Felblade:IsReady(15) and Player:Pain() <= 70 then
             return S.Felblade:Cast()
         end
 
         --actions.normal+=/fracture,if=soul_fragments<=3
-        if S.Fracture:IsReadyMorph() and SoulFragments <= 3 then
+        if S.Shear:IsReady("Melee") and SoulFragments <= 3 then
             return S.Fracture:Cast()
         end
 
         --actions.normal+=/fel_devastation
-        if S.FelDevastation:IsReady() then
+        if S.FelDevastation:IsReady(20) then
             return S.FelDevastation:Cast()
         end
 
         --actions.normal+=/sigil_of_flame
-        if S.SigilofFlame:IsReadyMorph() and Cache.EnemiesCount[8] >= 1 then
+        if Cache.EnemiesCount[8] > 0 and S.SigilofFlame:IsReadyMorph() then
             return S.SigilofFlame:Cast()
         end
 
         --actions.normal+=/shear
-        if S.Shear:IsReadyMorph("Melee") then
+        if S.Fracture:IsReady("Melee") then
             return S.Shear:Cast()
         end
 
         --actions.normal+=/throw_glaive
-        if S.ThrowGlaive:IsReady() then
+        if S.ThrowGlaive:IsReady('Melee') then
             return S.ThrowGlaive:Cast()
         end
     end
@@ -309,17 +312,17 @@ local function APL()
         return 0, 462338
     end
 
-    if S.Disrupt:IsReady() and RubimRH.InterruptsON() and Target:IsInterruptible() then
+    if S.Disrupt:IsReady(10) and RubimRH.InterruptsON() and Target:IsInterruptible() then
         return S.Disrupt:Cast()
+    end
+    if S.ConsumeMagic:IsReadyP() and Target:HasStealableBuff() then
+        return S.ConsumeMagic:Cast()
     end
 
     if QueueSkill() ~= nil then
         return QueueSkill()
     end
 
--- call_action_list,name=essences
-    local ShouldReturn = Essences(); if ShouldReturn and (true) then return ShouldReturn; end
-	
     --- Defensives
     if S.Metamorphosis:IsReady() and Player:HealthPercentage() <= RubimRH.db.profile[581].sk1 then
         return S.Metamorphosis:Cast()
