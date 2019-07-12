46a3a81d67a08e9463a534a19674cd9ef256189b
Ultrasn0w <skunkyfok@gmail.com>
2019-07-12 21:09:44 +0200

    [Druid] Balance CA & Lively spirit

--- Rubim-RH/Rotations/DR/balance.lua
+++ Rubim-RH/Rotations/DR/balance.lua
@@ -354,7 +354,7 @@ local function CDs ()
     end
     -- celestial_alignment,if=!buff.ca_inc.up&(buff.memory_of_lucid_dreams.up|(ap_check&astral_power>=40))&(!azerite.lively_spirit.enabled|buff.lively_spirit.up)&(dot.sunfire.remains>2&dot.moonfire.ticking&(dot.stellar_flare.ticking|!talent.stellar_flare.enabled))
     -- and (not Player:BuffP(CaInc())   
-   if S.CelestialAlignment:IsCastableP() and (Player:BuffP(S.MemoryOfLucidDreams) or (AP_Check(S.CelestialAlignment) and FutureAstralPower() >= 40)) and (not S.LivelySpirit:AzeriteEnabled() or Player:BuffP(S.LivelySpiritBuff)) and (Target:DebuffRemainsP(S.SunfireDebuff) > 2 and Target:DebuffP(S.MoonfireDebuff) and (Target:DebuffP(S.StellarFlareDebuff) or not S.StellarFlare:IsAvailable())) then
+   if S.CelestialAlignment:IsCastableP() and (Player:BuffP(S.MemoryOfLucidDreams) or (AP_Check(S.CelestialAlignment) and FutureAstralPower() >= 40)) and (Target:DebuffRemainsP(S.SunfireDebuff) > 2 and Target:DebuffP(S.MoonfireDebuff) and (Target:DebuffP(S.StellarFlareDebuff) or not S.StellarFlare:IsAvailable())) then
        return S.CelestialAlignment:Cast()
     end
 	-- fury_of_elune,if=(buff.celestial_alignment.up|buff.incarnation.up)|(cooldown.celestial_alignment.remains>30|cooldown.incarnation.remains>30)
