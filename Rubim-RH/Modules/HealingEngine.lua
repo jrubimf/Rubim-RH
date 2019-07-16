d256723666d944538bc5cd5d0385da27a3f3d654
Ultrasn0w <skunkyfok@gmail.com>
2019-07-16 17:32:06 +0200

    [Core] Fix error, cant compare boolean with number

--- Rubim-RH/Modules/HealingEngine.lua
+++ Rubim-RH/Modules/HealingEngine.lua
@@ -69,7 +69,7 @@ local function CanHeal(t)
     and not UnitIsCharmed(t)
 	and not UnitIsDeadOrGhost(t)
     and UnitIsConnected(t)
-	and not Player:DebuffRemainsP(S.DarkestDepths) > 0
+	and not Player:DebuffRemainsP(S.DarkestDepths)
  
 end
 
