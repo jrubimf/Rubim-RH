66dfde71ff80dbeac10e01f82717467df86682ec
roifok <skunkyfok@gmail.com>
2019-07-22 10:00:43 +0200

    [Warlock] Affliction Seed shards check

--- Rubim-RH/Rotations/WL/affli.lua
+++ Rubim-RH/Rotations/WL/affli.lua
@@ -372,7 +372,7 @@ local function APL()
     end
       -- potion
       -- seed_of_corruption,if=spell_targets.seed_of_corruption_aoe>=3
-      if S.SeedofCorruption:IsCastableP() and Player:DebuffDownP(S.SeedofCorruptionDebuff) and (EnemiesCount >= 3) then
+      if S.SeedofCorruption:IsCastableP() and Player:SoulShardsP() > 0 and Player:DebuffDownP(S.SeedofCorruptionDebuff) and (EnemiesCount >= 3) then
         return S.SeedofCorruption:Cast()
       end
       -- haunt
@@ -511,7 +511,7 @@ local function APL()
   
   Dots = function()
     -- seed_of_corruption,if=dot.corruption.remains<=action.seed_of_corruption.cast_time+time_to_shard+4.2*(1-talent.creeping_death.enabled*0.15)&spell_targets.seed_of_corruption_aoe>=3+raid_event.invulnerable.up+talent.writhe_in_agony.enabled&!dot.seed_of_corruption.remains&!action.seed_of_corruption.in_flight
-    if S.SeedofCorruption:IsCastableP() and (Target:DebuffRemainsP(S.CorruptionDebuff) <= S.SeedofCorruption:CastTime() + time_to_shard + 4.2 * (1 - num(S.CreepingDeath:IsAvailable()) * 0.15) and EnemiesCount >= 3 + num(S.WritheInAgony:IsAvailable()) and Target:DebuffDownP(S.SeedofCorruptionDebuff) and not S.SeedofCorruption:InFlight()) then
+    if S.SeedofCorruption:IsCastableP() and Player:SoulShardsP() > 0 and (Target:DebuffRemainsP(S.CorruptionDebuff) <= S.SeedofCorruption:CastTime() + time_to_shard + 4.2 * (1 - num(S.CreepingDeath:IsAvailable()) * 0.15) and EnemiesCount >= 3 + num(S.WritheInAgony:IsAvailable()) and Target:DebuffDownP(S.SeedofCorruptionDebuff) and not S.SeedofCorruption:InFlight()) then
       return S.SeedofCorruption:Cast()
     end
     -- agony,target_if=min:remains,if=talent.creeping_death.enabled&active_dot.agony<6&target.time_to_die>10&(remains<=gcd|cooldown.summon_darkglare.remains>10&(remains<5|!azerite.pandemic_invocation.rank&refreshable))
@@ -641,7 +641,7 @@ local function APL()
       local ShouldReturn = Fillers(); if ShouldReturn then return ShouldReturn; end
     end
     -- seed_of_corruption,if=variable.use_seed
-    if S.SeedofCorruption:IsCastableP() and (bool(VarUseSeed)) then
+    if S.SeedofCorruption:IsCastableP() and Player:SoulShardsP() > 0 and (bool(VarUseSeed)) then
       return S.SeedofCorruption:Cast()
     end
     -- unstable_affliction,if=!variable.use_seed&!prev_gcd.1.summon_darkglare&(talent.deathbolt.enabled&cooldown.deathbolt.remains<=execute_time&!azerite.cascading_calamity.enabled|(soul_shard>=5&spell_targets.seed_of_corruption_aoe<2|soul_shard>=2&spell_targets.seed_of_corruption_aoe>=2)&target.time_to_die>4+execute_time&spell_targets.seed_of_corruption_aoe=1|target.time_to_die<=8+execute_time*soul_shard)
