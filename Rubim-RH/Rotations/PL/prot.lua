be4b4efb148299570889971f0c5930544c976f71
Ultrasn0w <skunkyfok@gmail.com>
2019-07-28 10:27:59 +0200

    [Paladin] Protection Avenger Shield use on CD if not as Interrupt

--- Rubim-RH/Rotations/PL/prot.lua
+++ Rubim-RH/Rotations/PL/prot.lua
@@ -386,7 +386,7 @@ local function APL()
     end
 
     -- avengers_shield,if=cooldown_react
-    if S.AvengersShield:IsReadyP(30) and Target:AffectingCombat() and S.AvengersShield:CooldownUpP() and not RubimRH.ASInterruptON() then
+    if S.AvengersShield:IsReadyP(30) and Target:AffectingCombat() and not RubimRH.ASInterruptON() then
         return S.AvengersShield:Cast()
     end
 
