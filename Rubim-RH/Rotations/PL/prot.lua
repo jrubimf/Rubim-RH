7e1720b25084a2f975ce5fb4a9d36b871a368c93
Ultrasn0w <skunkyfok@gmail.com>
2019-07-28 11:22:45 +0200

    [Paladin] Protection Avenger Shield

--- Rubim-RH/Rotations/PL/prot.lua
+++ Rubim-RH/Rotations/PL/prot.lua
@@ -290,7 +290,10 @@ local function APL()
         end
     end
 
-    
+        -- avengers_shield,if=cooldown_react
+    if S.AvengersShield:IsReadyP(30) and RubimRH.TargetIsValid() and not RubimRH.ASInterruptON() then
+        return S.AvengersShield:Cast()
+    end
     if S.AvengersShield:IsReadyP(30) and Target:IsInterruptible() and RubimRH.ASInterruptON() then
         return S.AvengersShield:Cast()
     end
@@ -383,11 +386,6 @@ local function APL()
     -- judgment,if=(cooldown.judgment.remains<gcd&cooldown.judgment.charges_fractional>1&cooldown_react)|!talent.crusaders_judgment.enabled
     if S.Judgment:IsReadyP(30) and Target:AffectingCombat() and ((S.Judgment:CooldownRemains() < Player:GCD() and S.Judgment:ChargesFractional() > 1 and S.Judgment:CooldownUpP()) or not S.CrusadersJudgment:IsAvailable()) then
         return S.Judgment:Cast()
-    end
-
-    -- avengers_shield,if=cooldown_react
-    if S.AvengersShield:IsReadyP(30) and Target:AffectingCombat() and not RubimRH.ASInterruptON() then
-        return S.AvengersShield:Cast()
     end
 
     -- judgment,if=cooldown_react|!talent.crusaders_judgment.enabled
